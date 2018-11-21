aaf4cd461571f6a5d44240fcee19da7fd7baedc5
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2018-11-21 13:48:54 +0100

    java some practice

---
+++ JavaStdinStout.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class JavaStdinStout {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+
+        scan.close();
+        System.out.println(a);
+        
+        // Complete this line
+    }
+}
